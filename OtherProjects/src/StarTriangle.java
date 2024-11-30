a0730217a47f44ed3dc3a8479c5b8774fef93175
KuanNiisan <haibakhongtam2004@gmail.com>
2024-11-30 23:15:42 +0700

    Release flow in add-book-class branch

--- OtherProjects/src/StarTriangle.java
+++ OtherProjects/src/StarTriangle.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import java.util.Scanner;
 public class StarTriangle {
 	public static void main(String args[]) {
@@ -17,24 +17,5 @@ public class StarTriangle {
 			System.out.println();
 		}
 	}
-=======
-import java.util.Scanner;
-public class StarTriangle {
-	public static void main(String args[]) {
-		Scanner keyboard = new Scanner(System.in);
-		
-		System.out.println("Enter value n");
-		int n = keyboard.nextInt();
-		
-		for (int i = 1; i <= n; i++) {
-			for (int j = n - i; j > 0; j--) {
-				System.out.print(" ");
-			}
-			for (int k = 1; k <= 2*i - 1; k++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-	}
->>>>>>> ba21d7f3dc35a7036476dedf770956ff5cdfbf99
+
 }
--- OtherProjects/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
+++ OtherProjects/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package hust.soict.dsai.garbage;
 
 import java.util.Random;
@@ -36,43 +36,5 @@ public class ConcatenationInLoops {
         System.out.println("Using StringBuilder: " + (System.currentTimeMillis() - start) + "ms");
 
     }
-=======
-package hust.soict.dsai.garbage;
 
-import java.util.Random;
-
-public class ConcatenationInLoops {
-    public static void main(String[] args) {
-        Random r = new Random(123);
-
-        // Using + operator
-        long start = System.currentTimeMillis();
-        String s = "";
-        for (int i = 0; i < 65536; i ++) {
-            s += r.nextInt(2);
-        }
-        System.out.println("Using + operator: " + (System.currentTimeMillis() - start) + "ms");
-
-        // Using StringBuffer
-        r = new Random(123);
-        start = System.currentTimeMillis();
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < 65536; i ++) {
-            sb.append(r.nextInt(2));
-        }
-        s = sb.toString();
-        System.out.println("Using StringBuffer: " + (System.currentTimeMillis() - start) + "ms");
-
-        // Using StringBuilder
-        r = new Random(123);
-        start = System.currentTimeMillis();
-        StringBuilder sb2 = new StringBuilder();
-        for (int i = 0; i < 65536; i ++) {
-            sb2.append(r.nextInt(2));
-        }
-        s = sb2.toString();
-        System.out.println("Using StringBuilder: " + (System.currentTimeMillis() - start) + "ms");
-
-    }
->>>>>>> ba21d7f3dc35a7036476dedf770956ff5cdfbf99
 }
