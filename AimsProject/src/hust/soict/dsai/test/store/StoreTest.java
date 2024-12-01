8d6db32a9cf6c5d36de4588514efdff2d7d4d0f9
KuanNiisan <haibakhongtam2004@gmail.com>
2024-12-01 23:56:18 +0700

    Xong 11.

--- AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
+++ AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
@@ -15,16 +15,16 @@ public class StoreTest {
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
 		
 		// Test addDVD method
-        store.addDVD(dvd1);
-        store.addDVD(dvd2);
-        store.addDVD(dvd3);
+        store.addMedia(dvd1);
+        store.addMedia(dvd2);
+        store.addMedia(dvd3);
 
         // Print items in the store
         System.out.println("\nItems in store:");
         store.print();
 
         // Test removeDVD method
-        store.removeDVD(dvd2);
+        store.removeMedia(dvd2);
 
         // Print items in the store after removal
         System.out.println("\nItems in store after removal:");
