8699488bda0c04c829290258cb994a3b868acc54
KuanNiisan <haibakhongtam2004@gmail.com>
2024-12-01 18:29:54 +0700

    Xong 5.

--- AimsProject/src/hust/soict/dsai/aims/media/Book.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -6,7 +6,15 @@ public class Book extends Media {
 
 	private List<String> authors = new ArrayList<String>();
 	
-	
+	public Book(String title) {
+        super(title);
+    }
+    public Book(String title, String category) {
+        super(title, category);
+    }
+    public Book(String title, String category, float cost) {
+        super(title, category, cost);
+    }
 	
 	public void addAuthor(String authorName) {
         if (!authors.contains(authorName)) {
@@ -23,10 +31,5 @@ public class Book extends Media {
             System.out.println("No author has been found to remove!");
         }
     }
-
-
-	public Book() {
-		// TODO Auto-generated constructor stub
-	}
 
 }
--- AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -1,58 +1,29 @@
-
 package hust.soict.dsai.aims.media;
-public class DigitalVideoDisc extends Media{
-	private String director;
-	private int length;
-	private static int nbDigitalVideoDiscs = 0;
-	public DigitalVideoDisc(String title) {
-		super();
-		this.title = title;
-		this.id = ++nbDigitalVideoDiscs;
-	}
-	
-	public DigitalVideoDisc(String category, String title, float cost) {
-		super();
-		this.category = category;
-		this.title = title;
-		this.cost = cost;
-		this.id = ++nbDigitalVideoDiscs;
-	}
-	
-	public DigitalVideoDisc(String director, String category, String title, float cost) {
-		super();
-		this.director = director;
-		this.category = category;
-		this.title = title;
-		this.cost = cost;
-		this.id = ++nbDigitalVideoDiscs;
-	}
 
-	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.length = length;
-		this.cost = cost;
-		this.id = ++nbDigitalVideoDiscs;
-	}
-
-	public String getDirector() {
-		return director;
-	}
-	public int getLength() {
-		return length;
-	}
-	@Override
-    public String toString() {
-        return "DVD: " + this.title +
-                " - Category: " + this.category +
-                " - Director: " + this.title +
-                " - DVD length: " + this.length +
-                " - Cost: " + this.cost + "$";
-    }
+public class DigitalVideoDisc extends Disc{
 	
-	public boolean isMatch(String title) {
-		return this.title.toLowerCase().contains(title.toLowerCase());
+	    public DigitalVideoDisc(String title) {
+	        super(title);
+	    }    
+	    public DigitalVideoDisc(String title, String category, float cost) {
+	        super(title, category, cost);
+	    }
+	    public DigitalVideoDisc(String title, String category, String director, float cost) {
+	        super(title, category, director, cost);
+	    }
+	    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+	        super(title, category, director, length, cost);
+	    }
+	    @Override
+	    public String toString() {
+	        return "DVD: " + this.getTitle() +
+	                " - Category: " + this.getCategory() +
+	                " - Director: " + this.getDirector() +
+	                " - DVD length: " + this.getLength() +
+	                " - Cost: " + this.getCost() + "$";
+	    }
+
+	    public boolean isMatch(String title) {
+	        return this.getTitle().toLowerCase().contains(title.toLowerCase());
 	}
 }
